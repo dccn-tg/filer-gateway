27bda39d01c00ae0e9672d4338cb85c7bd87d13b
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-03-06 16:47:24 +0100

    Modified middleware to embed API document

--- pkg/swagger/gen.go
+++ pkg/swagger/gen.go
@@ -1,5 +1,4 @@
 package swagger
 
-//go:generate rm -rf server
 //go:generate mkdir -p server
 //go:generate swagger generate server --quiet --target server --name filer-gateway-api --spec swagger.yaml --exclude-main
