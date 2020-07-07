b811d76b8e822162ddf2cb744166f47e54b88d52
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-07-07 14:49:49 +0200

    print out env.sh to debug env. variables

--- docker-deploy-development.sh
+++ docker-deploy-development.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cat env.sh
+
 # export variables defined in env.sh
 set -a && source env.sh && set +a
 docker stack up -c docker-compose.yml -c docker-compose.swarm.yml --prune --with-registry-auth --resolve-image always filer-gateway
