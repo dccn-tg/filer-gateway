2aced6cd554d78fcd15b0b21d3df71ae2cec3e96
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-08-18 22:46:33 +0200

    Use https as default scheme

--- pkg/swagger/client/client/filer_gateway_client.go
+++ pkg/swagger/client/client/filer_gateway_client.go
@@ -25,7 +25,7 @@ const (
 )
 
 // DefaultSchemes are the default schemes found in Meta (info) section of spec file
-var DefaultSchemes = []string{"http"}
+var DefaultSchemes = []string{"https"}
 
 // NewHTTPClient creates a new filer gateway HTTP client.
 func NewHTTPClient(formats strfmt.Registry) *FilerGateway {
--- pkg/swagger/gen.go
+++ pkg/swagger/gen.go
@@ -5,4 +5,4 @@ package swagger
 //go:generate swagger generate server --quiet --target server --name filer-gateway --spec swagger.yaml --exclude-main -P models.Principle
 //go:generate rm -rf client/models client/client
 //go:generate mkdir -p client
-//go:generate swagger generate client --quiet --target client --name filer-gateway --spec swagger.yaml
+//go:generate swagger generate client --quiet --target client --name filer-gateway --default-scheme=https --spec swagger.yaml
