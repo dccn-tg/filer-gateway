d4669eb888492f2d817e79c2444dd8327aa2c29a
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-08-18 21:45:57 +0200

    Generated swagger client code

--- pkg/swagger/gen.go
+++ pkg/swagger/gen.go
@@ -3,3 +3,6 @@ package swagger
 //go:generate rm -rf server/models server/restapi
 //go:generate mkdir -p server
 //go:generate swagger generate server --quiet --target server --name filer-gateway --spec swagger.yaml --exclude-main -P models.Principle
+//go:generate rm -rf client/models client/client
+//go:generate mkdir -p client
+//go:generate swagger generate client --quiet --target client --name filer-gateway --spec swagger.yaml
