95db8565659ada8cbc526f81a508242085a32ee0
Hurng-Chun Lee <h.lee@donders.ru.nl>
2022-06-20 14:34:17 +0200

    removed debug message

--- internal/api-server/main.go
+++ internal/api-server/main.go
@@ -244,8 +244,6 @@ func main() {
 			return false
 		}
 
-		fmt.Printf("%s\n", scopes)
-
 		for _, scope := range scopes {
 			if !inScope(scope) {
 				return nil, errors.New(401, "token not in scope: %s", scope)
